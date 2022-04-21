aa97585fbd0a4628773206d2809b95f2dbe1ba5a
Cristian Ledezma <cristianledezma@MacBook-Pro-de-Cristian.local>
2022-04-20 18:47:37 -0600

    Update pop-up.js

--- ChallengeOracleONE-Encriptador-de-texto/js/pop-up.js
+++ ChallengeOracleONE-Encriptador-de-texto/js/pop-up.js
@@ -1,4 +1,4 @@
 function abrirVentanaAviso(){
-  var popUp = window.open('./pop-up.html','Aviso','toolbar=0,scrollbars=0,location=1,statusbar=0,menubar=0,resizable=0,width=600,height=100,top=center,left=center');
+  var popUp = window.open('./pop-up.html','Aviso','toolbar=0,scrollbars=0,location=0,statusbar=0,titlebar=0,status=0,menubar=0,resizable=0,width=600,height=100,top=center,left=center');
   setTimeout(function(){popUp.close()},2000);
 }
